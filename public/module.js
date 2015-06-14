4956f0a002cfffe0a3d705554eef6a9c089ad9eb
Loïc MARIE <loic.marie54@gmail.com>
2015-06-14 17:06:50 +0200

    Install Express + Angular + Redis + NodeJS

---
+++ public/module.js
@@ -0,0 +1,38 @@
+var mainmodule = angular.module('earnfrontend', ['ngRoute']);
+
+mainmodule.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/home',
+            controller: 'homeCtrl'
+        })
+        // .when('Fichiers', {
+        // 	templateUrl: '/fichiers',
+        // 	controller:
+        // })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+// mainmodule.controller('ListController', function($scope, $http) {
+    //   $scope.sendToServer = function(){
+    //       $http.post('/api',{'item':$scope.listitem})
+    //            .success(function(res){
+    //                getFromServer();
+    //            });
+    //   }
+    //   getFromServer();
+    //   function getFromServer(){
+    //       $http.get('/api')
+    //            .success(function(res){
+    //                $scope.items = res;
+    //                console.log(res);
+    //            });
+    //   }
+    // });
+
+
+mainmodule.controller('homeCtrl', function($scope) { 
+    
+});
